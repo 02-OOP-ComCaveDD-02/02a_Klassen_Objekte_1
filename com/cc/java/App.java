6da759ee8e6e056e180a977b151d9972829581cd
Peter <schmidt.multimedia@gmail.com>
2024-07-15 10:07:34 +0200

    02 / Referenzvariablen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -10,11 +10,17 @@ public class App {
         // cat --> Referenzvariable
         // Cat --> Referenztyp(Klasse)
         Cat cat = new Cat();
-        System.out.println(cat);
+        System.out.println("Blick von aussen: " + cat);
+        cat.tellYourAddress();
+
+        System.out.println("----------------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println("Blick von aussen: " + cat2);
+        cat2.tellYourAddress();
 
     }
 
    
-
 }
 
